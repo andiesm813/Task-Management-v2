878623beeac064729e9e5e804ae1dc3480eac59d
andiesm813 <86068582+andiesm813@users.noreply.github.com>
2021-10-11 14:07:38 -0300

    Task management v2 - Theme Flexible updates

--- src/app/master-view/master-view.module.ts
+++ src/app/master-view/master-view.module.ts
@@ -3,28 +3,28 @@ import { CommonModule } from '@angular/common';
 
 import { MasterViewRoutingModule } from './master-view-routing.module';
 import { MasterViewComponent } from './master-view.component';
-import { ListViewComponent } from './list-view/list-view.component';
-import { IgxIconModule, IgxAvatarModule, IgxListModule, IgxButtonModule, IgxCardModule, IgxNavbarModule, IgxToggleModule, IgxDropDownModule, IgxButtonGroupModule, IgxNavigationDrawerModule } from 'igniteui-angular';
-import { FormsModule } from '@angular/forms';
-import { KanbanViewComponent } from './kanban-view/kanban-view.component';
 import { ChildViewComponent } from './child-view/child-view.component';
+import { IgxCardModule, IgxButtonModule, IgxIconModule, IgxAvatarModule, IgxListModule, IgxNavbarModule, IgxToggleModule, IgxDropDownModule, IgxButtonGroupModule, IgxNavigationDrawerModule } from 'igniteui-angular';
+import { FormsModule } from '@angular/forms';
+import { ListViewComponent } from './list-view/list-view.component';
+import { KanbanViewComponent } from './kanban-view/kanban-view.component';
 
 @NgModule({
   declarations: [
     MasterViewComponent,
+    ChildViewComponent,
     ListViewComponent,
-    KanbanViewComponent,
-    ChildViewComponent
+    KanbanViewComponent
   ],
   imports: [
     CommonModule,
     MasterViewRoutingModule,
+    IgxCardModule,
+    IgxButtonModule,
     IgxIconModule,
+    FormsModule,
     IgxAvatarModule,
     IgxListModule,
-    IgxButtonModule,
-    FormsModule,
-    IgxCardModule,
     IgxNavbarModule,
     IgxToggleModule,
     IgxDropDownModule,
